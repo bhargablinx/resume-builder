58baeeaf46d27f058eeea1c9539711796138ad2d
BhargabLinx <bhargabbhuyan001@gmail.com>
2025-07-02 11:06:15 +0000

    Add resume builder page
    It container 2 section one for controlling the data in the resume and one section is to display the preview

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { NavBar } from "./components/NavBar";
+import { ResumeBuilder } from "./pages/ResumeBuilder";
 
 function App() {
     return (
         <>
             <NavBar />
+            <ResumeBuilder />
         </>
     );
 }
---
+++ src/pages/ResumeBuilder.jsx
@@ -0,0 +1,8 @@
+export const ResumeBuilder = () => {
+    return (
+        <section className="flex gap-2 h-[80vh] m-4 justify-between">
+            <div className="flex-1"></div>
+            <div className="border border-gray-300 shadow-2xl w-[595px] h-[842px]"></div>
+        </section>
+    );
+};
