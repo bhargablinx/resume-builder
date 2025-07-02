0a8aedf4d63a79b5722ded6e4abb2575d6a9869e
BhargabLinx <bhargabbhuyan001@gmail.com>
2025-07-02 13:38:19 +0000

    Clean the compoent and finalize the resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,21 +1,41 @@
 export const Resume = () => {
     return (
-        <div className="border border-gray-300 shadow-2xl w-[595px] min-h-[842px] h-auto py-4 px-8 flex flex-col gap-4">
+        <div className="border border-gray-300 shadow-2xl w-[595px] min-h-[842px] h-fit py-4 px-8 flex flex-col gap-4 bg-white">
             {/* Header */}
-            <div className="text-center">
+            <header className="text-center">
                 <div className="text-[26px] font-bold">BHARGAB BHUYAN</div>
-                <ul className="flex justify-center gap-2 text-[11px]">
+                <ul className="flex justify-center gap-2 text-[11px] list-none text-gray-800">
                     <li>Assam, India</li>
                     <li>&#8226;</li>
                     <li>bhargabbhuyan001@gmail.com</li>
                     <li>&#8226;</li>
-                    <li>GitHub</li>
+                    <li className="underline">
+                        <a
+                            href="https://github.com/bhargab001"
+                            target="_blank"
+                            rel="noreferrer"
+                            className="hover:underline"
+                        >
+                            GitHub
+                        </a>
+                    </li>
                     <li>&#8226;</li>
-                    <li>LinkedIn</li>
+                    <li className="underline">
+                        <a
+                            href="https://linkedin.com/in/your-profile"
+                            target="_blank"
+                            rel="noreferrer"
+                            className="hover:underline"
+                        >
+                            LinkedIn
+                        </a>
+                    </li>
                 </ul>
-            </div>
+            </header>
+
+            {/* SKILLS */}
             <SectionHeading sectionName="SKILLS">
-                <ul className="text-[11px] space-y-0.5">
+                <ul className="text-[11px] space-y-0.5 leading-tight tracking-tight">
                     <li>
                         <span className="font-bold">Web Technologies</span>:
                         HTML, CSS, JavaScript, TailwindCSS, ReactJS, Redux
@@ -26,13 +46,15 @@ export const Resume = () => {
                     </li>
                 </ul>
             </SectionHeading>
+
+            {/* PROJECTS */}
             <SectionHeading sectionName="PROJECTS">
                 <ProjectSection projectName="Demo Project 1">
-                    <div className="text-[11px]">
+                    <div className="text-[11px] leading-tight tracking-tight">
                         A résumé generator built with React. Dynamic and fully
-                        editable resumé generator that allows users to:
+                        editable résumé generator that allows users to:
                     </div>
-                    <ul className="list-disc list-inside space-y-0.5 text-[11px] ml-4 mt-1">
+                    <ul className="list-disc list-inside space-y-0.5 text-[11px] ml-4 mt-1 leading-tight tracking-tight">
                         <li>
                             Input and manage general information, education, and
                             work experience
@@ -48,11 +70,11 @@ export const Resume = () => {
                     </ul>
                 </ProjectSection>
                 <ProjectSection projectName="Demo Project 2">
-                    <div className="text-[11px]">
+                    <div className="text-[11px] leading-tight tracking-tight">
                         A résumé generator built with React. Dynamic and fully
-                        editable resumé generator that allows users to:
+                        editable résumé generator that allows users to:
                     </div>
-                    <ul className="list-disc list-inside space-y-0.5 text-[11px] ml-4 mt-1">
+                    <ul className="list-disc list-inside space-y-0.5 text-[11px] ml-4 mt-1 leading-tight tracking-tight">
                         <li>
                             Input and manage general information, education, and
                             work experience
@@ -68,11 +90,11 @@ export const Resume = () => {
                     </ul>
                 </ProjectSection>
                 <ProjectSection projectName="Demo Project 3">
-                    <div className="text-[11px]">
+                    <div className="text-[11px] leading-tight tracking-tight">
                         A résumé generator built with React. Dynamic and fully
-                        editable resumé generator that allows users to:
+                        editable résumé generator that allows users to:
                     </div>
-                    <ul className="list-disc list-inside space-y-0.5 text-[11px] ml-4 mt-1">
+                    <ul className="list-disc list-inside space-y-0.5 text-[11px] ml-4 mt-1 leading-tight tracking-tight">
                         <li>
                             Input and manage general information, education, and
                             work experience
@@ -88,41 +110,47 @@ export const Resume = () => {
                     </ul>
                 </ProjectSection>
             </SectionHeading>
-            <SectionHeading sectionName="Experience">
+
+            {/* EXPERIENCE */}
+            <SectionHeading sectionName="EXPERIENCE">
                 <JobSection
                     companyName="Microsoft"
-                    startData="Nov, 2019"
-                    endData="Sept, 2024"
+                    startDate="Nov, 2019"
+                    endDate="Sept, 2024"
                     jobRole="Jr. Software Engineer"
                 >
-                    <ul className="list-disc list-inside space-y-0.5 text-[11px] ml-4 mt-1">
+                    <ul className="list-disc list-inside space-y-0.5 text-[11px] ml-4 mt-1 leading-tight tracking-tight">
                         <li>
                             Developed and managed web applications using
                             ReactJS, Redux, RTK Query, and TypeScript.
                         </li>
                         <li>
-                            Migrated the front-end code base front CRA to VITE,
-                            helping the productivity of developers by reducing
-                            development server start time by 20% and production
-                            by 8%
+                            Migrated the front-end codebase from CRA to Vite,
+                            improving developer productivity and reducing build
+                            time.
                         </li>
                         <li>
-                            Streamlined and restructured Redux store, optimizing
-                            application's state management; reduced load time by
-                            25% and enhanced overall performance.
+                            Streamlined Redux store structure, reducing load
+                            time by 25% and improving performance.
                         </li>
                     </ul>
                 </JobSection>
             </SectionHeading>
-            <SectionHeading sectionName="Education">
+
+            {/* EDUCATION */}
+            <SectionHeading sectionName="EDUCATION">
                 <EducationSection
                     institute="Central Institute of Technology, Kokrajhar"
                     degree="Bachelor of Technology (B.Tech) - Computer Science"
                     startDate="Aug, 2021"
                     endDate="May, 2025"
                 >
-                    <ul className="list-disc list-inside space-y-0.5 text-[11px] ml-4 mt-1">
-                        <li>CGPA: 8.5 </li>
+                    <ul className="list-disc list-inside space-y-0.5 text-[11px] ml-4 mt-1 leading-tight tracking-tight">
+                        <li>CGPA: 8.5 (Till 6th Semester)</li>
+                        <li>
+                            Relevant Coursework: Data Structures, OS, CN, Web
+                            Dev
+                        </li>
                     </ul>
                 </EducationSection>
             </SectionHeading>
@@ -130,16 +158,23 @@ export const Resume = () => {
     );
 };
 
+// Shared section heading
 function SectionHeading({ children, sectionName }) {
     return (
-        <div>
-            <div className="text-[16px] font-bold mb-2">{sectionName}</div>
+        <section aria-labelledby={sectionName.toLowerCase()}>
+            <h2
+                id={sectionName.toLowerCase()}
+                className="text-[16px] font-bold mb-2"
+            >
+                {sectionName}
+            </h2>
             <hr className="mb-1" />
             {children}
-        </div>
+        </section>
     );
 }
 
+// Project section
 function ProjectSection({ children, projectName }) {
     return (
         <div className="my-2">
@@ -149,13 +184,14 @@ function ProjectSection({ children, projectName }) {
     );
 }
 
-function JobSection({ children, companyName, startData, endData, jobRole }) {
+// Job section
+function JobSection({ children, companyName, startDate, endDate, jobRole }) {
     return (
         <div className="my-2">
             <div className="flex justify-between items-center">
                 <div className="text-[14px] font-bold">{companyName}</div>
                 <div className="text-gray-600 text-[11px] italic tracking-wider">
-                    {startData} - {endData}
+                    {startDate} - {endDate}
                 </div>
             </div>
             <div className="text-[11px] font-semibold">{jobRole}</div>
@@ -164,6 +200,7 @@ function JobSection({ children, companyName, startData, endData, jobRole }) {
     );
 }
 
+// Education section
 function EducationSection({ institute, degree, startDate, endDate, children }) {
     return (
         <div className="my-2">
