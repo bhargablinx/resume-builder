4ca9052e92a830caf9fdf69e55adb0366ef54acb
BhargabLinx <bhargabbhuyan001@gmail.com>
2025-07-03 06:46:32 +0000

    Add initial State

--- src/slices/resumeSlice.js
+++ src/slices/resumeSlice.js
@@ -11,6 +11,57 @@ const resumeSlice = createSlice({
         projects: [
             {
                 projectName: "Demo Project 1",
+                description:
+                    "A résumé generator built with React. Dynamic and fully editable résumé generator that allows users to:",
+                bulletPoints: [
+                    "Input and manage general information, education, and work experience",
+                    "Edit and resubmit data with prefilled input fields for seamless updates",
+                    "View a live preview of the CV using conditional rendering and form state management",
+                ],
+            },
+            {
+                projectName: "Demo Project 2",
+                description:
+                    "A résumé generator built with React. Dynamic and fully editable résumé generator that allows users to:",
+                bulletPoints: [
+                    "Input and manage general information, education, and work experience",
+                    "Edit and resubmit data with prefilled input fields for seamless updates",
+                    "View a live preview of the CV using conditional rendering and form state management",
+                ],
+            },
+            {
+                projectName: "Demo Project 3",
+                description:
+                    "A résumé generator built with React. Dynamic and fully editable résumé generator that allows users to:",
+                bulletPoints: [
+                    "Input and manage general information, education, and work experience",
+                    "Edit and resubmit data with prefilled input fields for seamless updates",
+                    "View a live preview of the CV using conditional rendering and form state management",
+                ],
+            },
+        ],
+        experience: [
+            {
+                companyName: "Microsoft",
+                startDate: "Nov, 2019",
+                endDate: "Sept, 2024",
+                jobRole: "Jr. Software Engineer",
+                bulletPoints: [
+                    "Developed and managed web applications using ReactJS, Redux, RTK Query, and TypeScript.",
+                    "Migrated the front-end codebase from CRA to Vite, improving developer productivity and reducing build time.",
+                    "Streamlined Redux store structure, reducing load time by 25% and improving performance.",
+                ],
+            },
+        ],
+        education: [
+            {
+                institute: "Central Institute of Technology, Kokrajhar",
+                degree: "Bachelor of Technology (B.Tech) - Computer Science",
+                startDate: "Aug, 2021",
+                endDate: "May, 2025",
+                cgpa: "8.5",
+                otherInfo:
+                    "Relevant Coursework: Data Structures, OS, CN, Web Dev",
             },
         ],
     },
