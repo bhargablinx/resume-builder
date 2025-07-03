2757f6989de83a1ecddae1297b5707386e646f68
BhargabLinx <bhargabbhuyan001@gmail.com>
2025-07-03 04:38:41 +0000

    Add top button in the resume control component

--- src/components/ResumeControl.jsx
+++ src/components/ResumeControl.jsx
@@ -1,3 +1,17 @@
 export const ResumeControl = () => {
-    return <div className="flex-1"></div>;
+    return (
+        <div className="flex-1">
+            <div className="flex justify-between w-[300px] mx-auto">
+                <button className="bg-light-red hover:bg-light-red-hover text-light px-3 p-1 rounded-lg text-[14px] cursor-pointer">
+                    Clear Field
+                </button>
+                <button className="bg-green-500 hover:bg-green-600 px-3 p-1 rounded-lg text-[14px] cursor-pointer">
+                    Save
+                </button>
+                <button className="bg-blue-500 hover:bg-blue-600 text-light px-3 p-1 rounded-lg text-[14px] cursor-pointer">
+                    Load Sample
+                </button>
+            </div>
+        </div>
+    );
 };
