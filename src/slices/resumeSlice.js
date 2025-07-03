75243b1a158a45579ddd10d5cbb9fa922e9a195c
BhargabLinx <bhargabbhuyan001@gmail.com>
2025-07-03 06:48:09 +0000

    modified personal section

--- src/slices/resumeSlice.js
+++ src/slices/resumeSlice.js
@@ -3,11 +3,13 @@ import { createSlice } from "@reduxjs/toolkit";
 const resumeSlice = createSlice({
     name: "resume",
     initialState: {
-        name: "Bhargab Bhuyan",
-        address: "Assam, India",
-        email: "bhargabbhuyan001@gmail.com",
-        githubLink: "https://github.com/bhargablinx",
-        linkedInLink: "https://www.linkedin.com/in/bhargabb/",
+        personalInfo: {
+            name: "Bhargab Bhuyan",
+            address: "Assam, India",
+            email: "bhargabbhuyan001@gmail.com",
+            githubLink: "https://github.com/bhargablinx",
+            linkedInLink: "https://www.linkedin.com/in/bhargabb/",
+        },
         projects: [
             {
                 projectName: "Demo Project 1",
