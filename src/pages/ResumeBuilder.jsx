8dac0d9b51f27dd55584668e5a4378395bd8fa21
BhargabLinx <bhargabbhuyan001@gmail.com>
2025-07-28 04:13:42 +0000

    Add query param support

--- src/pages/ResumeBuilder.jsx
+++ src/pages/ResumeBuilder.jsx
@@ -1,11 +1,14 @@
 import { Resume } from "../components/Resume";
+import { useParams } from "react-router-dom";
 import { ResumeControl } from "../components/ResumeControl";
 
 export const ResumeBuilder = () => {
+    const param = useParams();
+
     return (
         <section className="flex gap-2 m-4 justify-between">
             <ResumeControl />
-            <Resume templateId={6} />
+            <Resume templateId={Number(param.id)} />
         </section>
     );
 };
